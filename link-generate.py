fe0e920136ddd843e1397b616e27f31872111f99
chenb0x <artofmorellc@gmail.com>
2019-03-12 15:13:29 -0700

    String conversion for menu selection

--- link-generate.py
+++ link-generate.py
@@ -11,8 +11,21 @@ def slug_generate(chartype):
 	"""
 	for a, b, c, d, e, f in itertools.product(chartype, chartype, chartype, chartype, chartype, chartype):
 		print(a + b + c + d + e + f)
-	
-choice = 0
+	pass
+
+choice = ""
+
+def print_menu():
+	print(30 * '-')
+	print("Is.Gd Slug Generator")
+	print(30 * '-')
+	print("is.gd generates a 6 character slug when shortening URLs.")
+	print("[1]: complete list (very long and not recommended)")
+	print("[2]: lowercase list (includes numbers in the generation)")
+	print("[3]: word pronouncable (ie ababab) DEFAULT")
+	print("[4]: numbers only (very unlikely to be productive)")
+	print("[5]: all caps (very unlikely to be productive)")
+	pass
 
 # Constants
 numbers = ['0','1','2','3','4','5','6','7','8','9']
@@ -26,31 +39,22 @@ wordpronounce = consonents + vowels
 complete = numbers + consonents + vowels + Cons + Vows
 
 
-print("is.gd generates a 6 character slug when shortening URLs.")
-print("1. complete list (very long and not recommended)")
-print("2. lowercase list (includes numbers in the generation)")
-print("3. word pronouncable (ie \"ababab\")")
-print("4. numbers only (very unlikely to be productive)")
-print("5. all caps (very unlikely to be productive)")
-choice = int(input("Select the type of link to generate: "))
+print_menu()
+choice = raw_input("Select the type of link to generate: ")
+choice = str(choice)
 
-if choice == "":
-	print("You did not make a selection!")
-elif choice == 1:
+if choice == "1":
 	slug_generate(complete)
-elif choice == 2:
+elif choice == "2":
 	slug_generate(lowernums)
-elif choice == 3:
+elif choice == "3":
 	for c1, v1, c2, v2, c3, v3 in itertools.product(consonents, vowels, consonents, vowels, consonents, vowels):
 		print(c1 + v1 + c2 + v2 + c3 + v3)
 	for v1, c1, v2, c2, v3, c3 in itertools.product(vowels, consonents, vowels, consonents, vowels, consonents):
 		print(v1 + c1 + v2 + c2 + v3 + c3)
-elif choice == 4:
+elif choice == "4":
 	slug_generate(numbers)
-elif choice == 5:
+elif choice == "5":
 	slug_generate(caps)
 else:
 	print("You did not make a selection!")
-
-
-
